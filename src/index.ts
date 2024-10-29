b724315318e7a959c5cbb547cbce4e2f9068d43b
Esteban-Bermudez <esteban@bermudezaguirre.com>
2024-10-28 20:14:55 -0700

    Update console.log to reflect actual url
    Express by default runs on 0.0.0.0 so this needed to be changed to
    reflect that

--- src/index.ts
+++ src/index.ts
@@ -165,5 +165,5 @@ function generateHtmlTemplate(data: any) {
 }
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port + baseUrl}`)
+  console.log(`Server running at http://0.0.0.0:${port + baseUrl}`)
 })
