c127e44a09458a3c5a3155fdd525516914385b53
Esteban-Bermudez <esteban@bermudezaguirre.com>
2024-11-14 16:00:08 -0800

    Implement the tmdbService to handle getting data
    This separates the logic for fetching and formatting the TMDB data into
    a separate service. This will make it easier to test the logic and make
    changes in the future.
    
    It also allows for only calling one function to do all the work for
    TMDB.

--- src/routes/itemAdded.ts
+++ src/routes/itemAdded.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
-import {tmdbData, TmdbResponse} from "../config/tmdbConfig";
 import { sendSendgridEmail } from "../services/sendgridService";
 import { sendSMTPEmail } from "../services/smtpService";
+import { getTmdbData } from "../services/tmdbService";
 
 const router = express.Router();
 
@@ -14,7 +14,7 @@ router.post("/", async (req: Request, res: Response) => {
 
   let tmdbResponse
   try {
-    tmdbResponse = await fetchTmdbData(req.body.Provider_tmdb)
+    tmdbResponse = await getTmdbData(req.body.Provider_tmdb)
   } catch (error: any) {
     console.error(error)
     const status = error.status || 500
@@ -22,52 +22,18 @@ router.post("/", async (req: Request, res: Response) => {
     res.status(status).json({ message })
     return
   }
-  const formattedResponse = formatTmdbResponse(tmdbResponse)
 
   if (process.env.MAIL_PROVIDER?.toLowerCase() === "smtp") {
-    await sendSMTPEmail(formattedResponse)
+    await sendSMTPEmail(tmdbResponse)
   } else {
-    await sendSendgridEmail(formattedResponse)
+    await sendSendgridEmail(tmdbResponse)
   }
-  res.status(200).json(formattedResponse)
+  res.status(200).json(tmdbResponse)
 })
 
 function printRequest(req: Request) {
   console.log("Headers: ", req.headers)
   console.log("Body: ", req.body)
-}
-
-async function fetchTmdbData(tmdbId: string) {
-  const url: string = `https://api.themoviedb.org/3/movie/${tmdbId}?api_key=${tmdbData.apiKey}`
-  const tmdbResponse = await fetch(url).then((response) => {
-    if (!response.ok) {
-      throw {
-        status: response.status,
-        message: "TMDB ERROR: " + response.statusText,
-      }
-    }
-    return response.json()
-  })
-
-  return tmdbResponse
-}
-
-function formatTmdbResponse(response: any): TmdbResponse {
-  return {
-    genres: response.genres
-      .map((genre: { id: number; name: string }) => genre.name)
-      .join(", "),
-    homepage: response.homepage,
-    id: response.id,
-    overview: response.overview,
-    posterPath: `${tmdbData.posterUrl}${response.poster_path}`,
-    releaseDate: response.release_date,
-    runtime: response.runtime,
-    tagline: response.tagline,
-    title: response.title,
-    movieUrl: `${tmdbData.movieUrl}${response.id}`,
-    imdbUrl: `${tmdbData.imdbUrl}${response.imdb_id}`,
-  }
 }
 
 export default router;
---
+++ src/services/tmdbService.ts
@@ -0,0 +1,34 @@
+import { tmdbData, TmdbResponse } from "../config/tmdbConfig"
+
+export async function getTmdbData(tmdbId: string) {
+  const url: string = `https://api.themoviedb.org/3/movie/${tmdbId}?api_key=${tmdbData.apiKey}`
+  const tmdbResponse = await fetch(url).then((response) => {
+    if (!response.ok) {
+      throw {
+        status: response.status,
+        message: "TMDB ERROR: " + response.statusText,
+      }
+    }
+    return response.json()
+  })
+
+  return formatTmdbResponse(tmdbResponse)
+}
+
+export function formatTmdbResponse(response: any): TmdbResponse {
+  return {
+    genres: response.genres
+      .map((genre: { id: number; name: string }) => genre.name)
+      .join(", "),
+    homepage: response.homepage,
+    id: response.id,
+    overview: response.overview,
+    posterPath: `${tmdbData.posterUrl}${response.poster_path}`,
+    releaseDate: response.release_date,
+    runtime: response.runtime,
+    tagline: response.tagline,
+    title: response.title,
+    movieUrl: `${tmdbData.movieUrl}${response.id}`,
+    imdbUrl: `${tmdbData.imdbUrl}${response.imdb_id}`,
+  }
+}
