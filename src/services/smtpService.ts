1d3841e44c89805012b2e621b8fe2e1f64aff699
Esteban-Bermudez <esteban@bermudezaguirre.com>
2025-06-22 12:52:51 -0700

    Add better SMTP support for itemAdded route
    This makes it so that we dynamically generate the subject line based on
    the item type (Movie, Season, Episode) and include the name and release
    year in the subject. The HTML template is also updated to better format
    the email content. This helps to match it with what we have in
    SendGrid.

--- src/services/smtpService.ts
+++ src/services/smtpService.ts
@@ -14,14 +14,23 @@ export async function sendSMTPEmail(formattedResponse: MailfinResponse) {
     },
   })
 
+  let subject = ''
+  let html = generateHtmlTemplate(formattedResponse)
+  if (formattedResponse.itemType === "Movie") {
+    subject = `A new movie has been added to Jellyfin: ${formattedResponse.name} (${formattedResponse.releaseYear})`
+  } else if (formattedResponse.itemType === "Season") {
+    subject = `A new season has been added to Jellyfin: ${formattedResponse.name}`
+  } else if (formattedResponse.itemType === "Episode") {
+    subject = `A new episode has been added to Jellyfin: ${formattedResponse.name}`
+  } else {
+    subject = `A new item has been added to Jellyfin: ${formattedResponse.name}`
+  }
+    
   const mailOptions = {
     from: process.env.SMTP_SENDER_EMAIL,
     to: process.env.SMTP_RECEIVER_EMAIL,
-    subject:
-      ` A new ${formattedResponse.itemType} has been added to Jellyfin ` +
-      formattedResponse.name +
-      ` (${formattedResponse.releaseYear})`,
-    html: generateHtmlTemplate(formattedResponse),
+    subject: subject,
+    html: html,
   }
 
   await transporter.sendMail(mailOptions)
